c09cc834b20dfae1793d47a9c25cd158e416014b
sadmansakib <sadmansakib@sadman.space>
2025-07-09 15:53:53 +0600

    fix: Update sampleMatch regex and improve IsSampleFile function for better filename handling

--- internal/utils/regex.go
+++ internal/utils/regex.go
@@ -9,7 +9,7 @@ import (
 var (
 	videoMatch  = "(?i)(\\.)(webm|m4v|3gp|nsv|ty|strm|rm|rmvb|m3u|ifo|mov|qt|divx|xvid|bivx|nrg|pva|wmv|asf|asx|ogm|ogv|m2v|avi|bin|dat|dvr-ms|mpg|mpeg|mp4|avc|vp3|svq3|nuv|viv|dv|fli|flv|wpl|img|iso|vob|mkv|mk3d|ts|wtv|m2ts)$"
 	musicMatch  = "(?i)(\\.)(mp2|mp3|m4a|m4b|m4p|ogg|oga|opus|wma|wav|wv|flac|ape|aif|aiff|aifc)$"
-	sampleMatch = `(?i)(^|[\s/\\])(sample|trailer|thumb|special|extras?)s?[-/]|(\((sample|trailer|thumb|special|extras?)s?\))|(-\s*(sample|trailer|thumb|special|extras?)s?)`
+	sampleMatch = `(?i)(^|[\s._-])(sample|trailer|thumb|special|extras?)s?[\s._-]|(\((sample|trailer|thumb|special|extras?)s?\))|[\s._-](sample|trailer|thumb|special|extras?)s?(\.|$)`
 )
 
 var (
@@ -51,8 +51,9 @@ func IsMediaFile(path string) bool {
 }
 
 func IsSampleFile(path string) bool {
-	if strings.HasSuffix(strings.ToLower(path), "sample.mkv") {
+	filename := filepath.Base(path)
+	if strings.HasSuffix(strings.ToLower(filename), "sample.mkv") {
 		return true
 	}
-	return RegexMatch(sampleRegex, path)
+	return RegexMatch(sampleRegex, filename)
 }
